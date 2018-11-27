f7d2a31da41c79ebaa3b8ed5fec2e1900df60668
Than Ke <than.nguyenke@hyakuren.org>
2018-11-27 15:08:55 +0700

    Test edit commit

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,8 @@ class UserController < ApplicationController
   def show
     @users = [{name: "Than", address: "Binh Dinh"}, {name: "Phu", address: "Thanh Hoa"}]
   end
+
+  def index
+    
+  end
 end
